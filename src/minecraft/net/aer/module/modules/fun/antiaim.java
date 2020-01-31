e5ad2ce416d9d3ef25f75dda07907860d514129b
ProgrammingCatss <48096795+ProgrammingCatss@users.noreply.github.com>
2020-01-31 13:39:25 +0200

    Create antiaim.java

---
+++ src/minecraft/net/aer/module/modules/fun/antiaim.java
@@ -0,0 +1,15 @@
+
+package net.aer.module.modules.combat;
+
+import net.aer.module.Category;
+import net.aer.module.Module;
+import net.aer.utils.valuesystem.BooleanValue;
+
+public class AntiAim extends Module {
+
+	public AntiAim() {
+		super("AntiAim", Category.FUN, "Makes your head move as you wish!");
+	}
+
+
+}
